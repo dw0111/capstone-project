ae182baccd9e3b860a636dbcf55a80c640ddb8ac
Daniel Wiesner <daniel_wiesner@gmx.de>
2021-03-08 15:31:25 +0100

    Install uuid

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
 
+import GlobalStyles from '../src/GlobalStyles'
+import { addDecorator } from '@storybook/react';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+function withGlobalStyles(storyFn) {
+  return (
+    <>
+      <GlobalStyles />
+      {storyFn()}
+    </>
+  );
+}
+
+addDecorator(withGlobalStyles);
